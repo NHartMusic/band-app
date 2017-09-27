3a8d5f796a9fbf56a7dea6a0a2accb0c5979ba61
Nick Hart <nhartmusic@gmail.com>
2017-09-26 20:10:49 -0700

    index.html file links and pages folder in client

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Login from "./components/Login/Login";
+
+class Login extends Component{
+  constructor(){
+    super();
+    this.state = {
+
+    }
+    this.logUserIn = this.logUserIn.bind(this);
+    this.logUserOut = this.logUserOut.bind(this);
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Login;
