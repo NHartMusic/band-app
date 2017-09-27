5a7883cec784457381dc7c05e385e35e2e427ccb
unknown <ashishuiux@gmail.com>
2017-09-26 21:11:44 -0700

    add page and component layout structure

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ app.use(bodyParser.json());
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  if(process.env.NODE_ENV === "production"){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  }else{
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  }
 });
 
 app.listen(PORT, function() {
