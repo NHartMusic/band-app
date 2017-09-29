fa656a11131552f1157ae13c36e02f4fc805ed47
unknown <ashishuiux@gmail.com>
2017-09-28 21:14:52 -0700

    fix seed data query

--- scripts/bandSeeds.js
+++ scripts/bandSeeds.js
@@ -169,14 +169,15 @@ const BandSeed = [
 
   }
 ];
+
 Bands
   .remove({})
   .then(() => Bands.collection.insertMany(BandSeed))
   .then(data => {
     console.log(data.insertedIds.length + " records inserted!");
-    process.exit(0);
+    // process.exit(0);
   })
   .catch(err => {
     console.error(err);
-    process.exit(1);
+    // process.exit(1);
   });
