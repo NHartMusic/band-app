b6349db7e141a5b6ebae707b7f5c8944d58569a9
unknown <ashishuiux@gmail.com>
2017-10-04 21:34:58 -0700

    delete bands on manage bands page

--- client/src/components/DisplayGenres/DisplayGenres.js
+++ client/src/components/DisplayGenres/DisplayGenres.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import API from '../../utils/API';
+import API from '../../utils/API';
 import './DisplayGenres.css';
 
 class DisplayGenres extends Component{
@@ -15,12 +15,25 @@ class DisplayGenres extends Component{
     window.location.pathname = `/profile/${event.target.id}`;
   }
 
+  handleDelete = (event) => {
+    // console.log("Boom! You're gone");
+    // console.log(event.target.id + " you're gone");
+    API.deleteBand(event.target.id)
+        .then(window.location.pathname="/manage-bands")
+        .catch(err => console.log(err));
+  }
+
   render(){
     return(
       <div className="row">
         {this.props.bandData.map(x => {
           return(
             <div className="oneBand" key={x._id}>
+            {this.props.deleteBand? <button
+              className="delete_buttons"
+              onClick={this.handleDelete}
+              id={x._id}>X</button> :
+             <p></p>}
               <h3>{x.name}</h3>
               <p>Genre: <em>{x.genre}</em></p>
               <img
